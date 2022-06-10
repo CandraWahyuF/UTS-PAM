1c343d4b11271cc7a31a0eece4b68e71bf3686bc
candrawahyuf <83354350+CandraWahyuF@users.noreply.github.com>
2022-06-10 09:31:01 +0700

    hapus

--- src/database/Data.jsx
+++ src/database/Data.jsx
@@ -1,20 +1,28 @@
 const JADWAL = [
-    {
-        jadwal_id: "1",
-        dermaga_keberangkatan: "Merak",
-        dermaga_tujuan: "Bakauheni",
-        kelas_layanan: "Reguler", 
-        tanggal_masuk: "Sabtu, 26 Maret 2022",
-        jam_masuk: "15.30 WIB"
-    },
-    {
-        jadwal_id: "3",
-        dermaga_keberangkatan: "Bakauheni",
-        dermaga_tujuan: "Merak",
-        kelas_layanan: "Reguler", 
-        tanggal_masuk: "Minggu, 27 Maret 2022",
-        jam_masuk: "15.30 WIB"
-    },
+  {
+    jadwal_id: "1",
+    dermaga_keberangkatan: "Merak",
+    dermaga_tujuan: "Bakauheni",
+    kelas_layanan: "Reguler",
+    tanggal_masuk: "Sabtu, 26 Maret 2022",
+    jam_masuk: "16.00 WIB",
+  },
+  {
+    jadwal_id: "2",
+    dermaga_keberangkatan: "Bakauheni",
+    dermaga_tujuan: "Panjang",
+    kelas_layanan: "Reguler",
+    tanggal_masuk: "Senin, 27 Maret 2022",
+    jam_masuk: "12.00 WIB",
+  },
+  {
+    jadwal_id: "3",
+    dermaga_keberangkatan: "Bakauheni",
+    dermaga_tujuan: "Merak",
+    kelas_layanan: "Reguler",
+    tanggal_masuk: "Minggu, 28 Maret 2022",
+    jam_masuk: "15.00 WIB",
+  },
 ];
 
-export { JADWAL }
+export { JADWAL };
